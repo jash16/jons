822869d9a0a85d479fdb4ba203e282b9d62a8b35
jash16 <jash16@163.com>
2015-11-06 16:40:51 +0800

    db

--- server/db.go
+++ server/db.go
@@ -2,6 +2,7 @@ package server
 
 import (
     "sync"
+//    "container/list"
 )
 
 const (
@@ -23,6 +24,7 @@ const (
 
 type Key struct {
     Type int32
+    Ref int32
     Value string
 }
 
@@ -37,15 +39,21 @@ func NewElement(typ int32, value interface{}) *Element {
     return &Element {
         Type: typ,
         Encode: JON_ENCODING_RAW,
-        Value:
+        Value: value,
     }
 }
 
 func (e *Element) Copy() *Element {
-
+    switch e.Value.(type) {
+    case string:
+    case map[string]string: //use for hash type
+    case map[string]byte:   //use for set and sorted set
+    case [][]byte: //use for list
+    }
 }
+
 type DB struct {
-    DataMap map[Element]Element
+    DataMap map[Key]Element
     sync.RWMutex
 }
 
--- server/server.go
+++ server/server.go
@@ -2,8 +2,11 @@ package server
 
 import (
     "common"
+    "protocol"
+    "os"
     "log"
     "net"
+    "sync"
 )
 
 type Server struct {
@@ -13,6 +16,8 @@ type Server struct {
     Opts *ServerOption
     logger Logger
 
+    wg common.WaitGroupWrapper
+    sync.Mutex
     exitChan chan bool
 }
 
@@ -20,16 +25,42 @@ func NewServer(opt *ServerOption) *Server {
     return &Server {
         logger: log.New(os.Stderr, "[jon_server]", log.Ldate|log.Ltime|log.Lmicroseconds),
         Opts: opt,
+        exitChan: make(chan bool)
     }
 }
 
 func (s *Server) Main() {
+    var dbs []*JobDb
+    for i := 0; i < s.Opts.DbNum; i ++ {
+        db := NewJonDb()
+        dbs = append(dbs, db)
+    }
+    s.db = dbs
 
+    tcpListener, err := net.Listen("tcp", s.Opts.TCPAddr)
+    if err != nil {
+        s.logf("listen %s error - %s", s.Opts.TCPAddr, err)
+        os.Exit(1)
+    }
+    s.tcpListener = tcpListner
+    ctx := &context{s: s}
+    tcpSrv := &tcpServer {
+        ctx: ctx,
+    }
+
+    s.wg.Wrap(func() {
+        protocol.TCPServer(s.tcpListener, tcpSrv, s.logger)
+    })
 }
 
 func (s *Server) logf(data string, args...interface{}) {
     s.logger.Output(2, fmt.Sprintf(data, args...))
 }
 
-func Exit() {
+func (s *Server) Exit() {
+    if s.tcpListener != nil {
+        s.tcpListener.Close()
+    }
+    close(s.exitChan)
+    s.Wait()
 }
