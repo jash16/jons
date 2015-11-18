1ba50aa443bf590d1ea3948f9ef3fe6fbf5e0206
jash16 <jash16@163.com>
2015-11-18 14:58:05 +0800

    set

--- server/t_set.go
+++ server/t_set.go
@@ -1,1 +1,26 @@
 package server
+
+import (
+    "fmt"
+)
+
+func (s *Server) Sadd(cli *Client) error {
+    if cli.argc <= 2 {
+         cli.ErrorResponse(wrongArgs, "sadd")
+         return nil
+    }
+
+    var resp string
+
+    return nil
+}
+
+func (s *Server) Srem(cli *Client) error {
+
+    return nil
+}
+
+func (s *Server) Sinter(cli *Client) error {
+
+    return nil
+}
